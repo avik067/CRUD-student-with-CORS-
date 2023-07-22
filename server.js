e08a7ddcc794a69a47ad56bd491e945d3c0fd5df
avik067 <avikbiswas067@gmail.com>
2023-07-22 14:21:12 +0530

    working

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require ('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
+app.use(cors({
+    origin:"http://localhost:3004"
+}))
 app.use(express.json())
 
 // const  PORT = process.env.PORT || 4000 // either on 4000 or any 
